e6878bd3ac1678a49b28ea9cc5049e18e8921b0b
mfrancia <m.francia@unibo.it>
2026-05-06 23:16:21 +0200

    chore: working on CI

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -exo
 jupyter nbconvert --execute --to notebook --inplace get-bib.ipynb
 jupyter nbconvert --execute --to notebook --inplace scopus-citations.ipynb
-jupyter nbconvert --execute --to notebook --inplace scholar-citations.ipynb
+jupyter nbconvert --execute --to notebook --inplace scholar-citations-serpapi.ipynb
 jupyter nbconvert --execute --to notebook --inplace merge.ipynb
