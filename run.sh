f0c485df6d73866175421454d53be0f813789b7f
mfrancia <m.francia@unibo.it>
2026-05-06 22:56:44 +0200

    fix: add serpapi

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -exo
-if [[ -d venv/Scripts ]]; then
-    source venv/Scripts/activate
-else
-    source venv/bin/activate
-fi
-
+jupyter nbconvert --execute --to notebook --inplace get-bib.ipynb
 jupyter nbconvert --execute --to notebook --inplace scopus-citations.ipynb
 jupyter nbconvert --execute --to notebook --inplace scholar-citations.ipynb
 jupyter nbconvert --execute --to notebook --inplace merge.ipynb
